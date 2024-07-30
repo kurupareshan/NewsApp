073899cdda2a6eca1321ea89bd11172acc6898d2
kurupareshan <kuru.cameraxis@gmail.com>
2024-07-30 10:07:08 +0530

    Initial Commit

---
+++ NewsApp/NewsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsAppApp.swift
+//  NewsApp
+//
+//  Created by kurupareshan pathmanathan on 7/30/24.
+//
+
+import SwiftUI
+
+@main
+struct NewsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
